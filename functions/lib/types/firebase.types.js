6ab41927a6b6277ffb264d2040bd43b6a25a5455
paulamadry-netizen <paul.amadry@gmail.com>
2025-12-27 01:23:24 +0100

    vitrine: instant funding payment links

--- functions/lib/types/firebase.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Type definitions for Firebase/Firestore data structures
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=firebase.types.js.map
--- functions/lib/types/yahoo.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Type definitions for Yahoo Finance API responses
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=yahoo.types.js.map
